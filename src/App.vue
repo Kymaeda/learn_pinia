5539a17490832d0c521152729d1d5142275e8fe9
Kymaeda <kyo.m.720@gmail.com>
2022-09-24 09:55:46 +0900

    read/update state value from components
    - pinia is enable to update state value directly from components
    - comparing vuex, its more simple to handle store

--- src/App.vue
+++ src/App.vue
@@ -7,4 +7,8 @@ const store = useCounterStore()
 <template>
   <!-- NOTE: enabled to direct access to state value with `store` -->
   <div>Count: {{ store.count }}</div>
+  <div>
+    <!-- NOTE: enable to update state value directly -->
+    <button @click="store.count++">increment</button>
+  </div>
 </template>
